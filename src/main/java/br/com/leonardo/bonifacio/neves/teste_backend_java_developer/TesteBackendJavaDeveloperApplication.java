f805aa46c9c5474a040a7a4ef526435dc5296c0e
Leonardo Bonifacio Neves <lbonifacioneves@gmail.com>
2024-02-04 10:42:11 -0300

    Initial Files

---
+++ src/main/java/br/com/leonardo/bonifacio/neves/teste_backend_java_developer/TesteBackendJavaDeveloperApplication.java
@@ -0,0 +1,13 @@
+package br.com.leonardo.bonifacio.neves.teste_backend_java_developer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteBackendJavaDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteBackendJavaDeveloperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leonardo/bonifacio/neves/teste_backend_java_developer/TesteBackendJavaDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leonardo.bonifacio.neves.teste_backend_java_developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteBackendJavaDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
